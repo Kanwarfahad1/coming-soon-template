334b207da87a27ebb1f31d27d2e6094610177ed2
Kanwarfahad1 <kanwarfahad1@gmail.com>
2020-02-23 15:55:31 +0500

    Responsive issues fixed

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ jQuery(function () {
     jQuery(".digits").countdown({
         image: "assets/images/digits.png",
         format: "dd:hh:mm:ss",
-        endTime: new Date(2013, 12, 2)
+        endTime: new Date(2020, 12, 2)
     });
 });
 
